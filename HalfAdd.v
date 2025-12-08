69a4691493b89e6d340a7550b14f3f2b8779b732
vkarthi-20 <vkarthi20vks@gmail.com>
2025-12-08 18:35:36 +0530

    Add files via upload

---
+++ HalfAdd.v
@@ -0,0 +1,11 @@
+module HalfAdd (
+    input  wire a, b,     // Inputs
+    output wire sum,      // Sum output
+    output wire carry     // Carry output
+);
+
+    // Logic equations
+    assign sum   = a ^ b;   // XOR for sum
+    assign carry = a & b;   // AND for carry
+
+endmodule
